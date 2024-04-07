d3f3e438c944e5bd02d831956b4675471c8e3f9a
Artem Kolpakov <artemkolpakov78@gmail.com>
2024-04-06 18:30:29 -0700

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Artem Kolpakov on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
